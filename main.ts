ae48b804fce5a7783487e227d92ae8c9e2b7da0d
1000zoo <cjswldn99@gmail.com>
2024-03-14 23:39:41 +0900

    feat: 기능 구현 완료 - TODO - '갅'와 같은 받침에 대한 처리가 필요 - 에러에 대한 처리

--- main.ts
+++ main.ts
@@ -1,134 +1,142 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+export default class KrEngTypoFixer extends Plugin {
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+	onload() {
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			id: "fix-to-korean",
+			name: "Fix to Korean",
+			editorCallback: (editor: Editor) => {
+				const selected = editor.getSelection()
+				if (!selected) return; // 드레그 된 부분이 없다면 끝
+				const fixed = fixTypo(selected);
+				editor.replaceSelection(fixed)
+
+				console.log(selected)
+				console.log(fixed)
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		})
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+const start = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
+const middle = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"];
+const end = [null, "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
+
+
+const engKrMap: { [key: string]: string} = {
+    "a" : "ㅁ", "b" : "ㅠ", "c" : "ㅊ", "d" : "ㅇ", "e" : "ㄷ", "f" : "ㄹ", "g" : "ㅎ", "h" : "ㅗ", "i" : "ㅑ", "j" : "ㅓ",
+    "k" : "ㅏ", "l" : "ㅣ", "m" : "ㅡ", "n" : "ㅜ", "o" : "ㅐ", "p" : "ㅔ", "q" : "ㅂ", "r" : "ㄱ", "s" : "ㄴ", "t" : "ㅅ",
+    "u" : "ㅕ", "v" : "ㅍ", "w" : "ㅈ", "x" : "ㅌ", "y" : "ㅛ", "z" : "ㅋ", "E" : "ㄸ", "Q" : "ㅃ", "R" : "ㄲ", "T" : "ㅆ",
+    "W" : "ㅉ", "O" : "ㅒ", "P" : "ㅖ"
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+function fixTypo(inputString: string): string {
+    const hangulList = toHangulList(inputString);
+    const split = splitHangul(hangulList);
+    const concat = split.map((w) => concateHangul(w));
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    return concat.join("");
 }
+
+function toHangulList(engs: string): string {
+	let temp: string[] = [];
+
+	for (let alp of engs) {
+		temp.push(engKrMap[alp] || alp);
+	}
+
+	return temp.join("");
+}
+
+function splitHangul(hanguls: string): string[][] {
+    let splitList: string[][] = [];
+    let temp: string[] = [];
+
+    for (let i = 0; i < hanguls.length; i++) {
+        const hangul = hanguls[i];
+
+        // 한글이 아닌 경우
+        if (!isHangul(hangul)) {
+            if (temp.length !== 0) {
+                splitList.push([...temp]);
+                temp = [];
+            }
+            splitList.push([hangul]);
+            continue;
+        }
+
+        // 마지막 문자인 경우
+        if (i === hanguls.length - 1) {
+            temp.push(hangul);
+            splitList.push([...temp]);
+            continue;
+        }
+
+        // 현재까지 임시 배열에 저장된 한글 자모가 2개 이하인 경우
+        if (temp.length <= 1) {
+            temp.push(hangul);
+            continue;
+        }
+
+        // 현재 글자와 다음 글자가 모두 중성이 아닌 경우 (종성이 있는 경우)
+        if (!isMiddle(hangul) && !isMiddle(hanguls[i + 1])) {
+            temp.push(hangul);
+            splitList.push([...temp]);
+            temp = [];
+        } else if (!isMiddle(hangul)) {
+            // 현재 글자가 중성이 아니면 현재까지의 배열을 결과에 추가하고, 현재 글자로 새 배열을 시작
+            splitList.push([...temp]);
+            temp = [hangul];
+        } else {
+            // 그 외의 경우 현재 글자를 임시 배열에 추가
+            temp.push(hangul);
+        }
+    }
+
+    return splitList;
+}
+
+function concateHangul(hangul: string[]): string {
+    if (!onlyHangul(hangul)) {
+        return hangul.join("");
+    }
+
+    // 종성이 없는 경우
+    if (hangul.length === 2) {
+        return String.fromCharCode((start.indexOf(hangul[0]) * 588) + (middle.indexOf(hangul[1]) * 28) + 44032);
+    }
+
+    // 종성이 있는 경우
+    if (hangul.length === 3) {
+        return concateHangulBadchim(hangul);
+    }
+
+    return "";
+}
+
+function concateHangulBadchim(hangul: string[]): string {
+    return String.fromCharCode(
+        (start.indexOf(hangul[0]) * 588) +
+        (middle.indexOf(hangul[1]) * 28) +
+        (end.indexOf(hangul[2]) || 0) + 44032
+    );
+}
+
+function onlyHangul(hangul: string[]): boolean {
+    return hangul.every(h => isHangul(h));
+}
+
+function isHangul(c: string): boolean {
+    // 초성, 중성, 종성 리스트에 문자 c가 포함되어 있는지 확인합니다.
+    return start.includes(c) || middle.includes(c) || end.includes(c);
+}
+
+function isMiddle(hangul: string): boolean {
+    // 중성 리스트에 문자 hangul이 포함되어 있는지 확인합니다.
+    return middle.includes(hangul);
+}
+
