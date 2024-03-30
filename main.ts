3e3abf47e1101a0336e1c944bd0009459b8e65e8
1000zoo <cjswldn99@gmail.com>
2024-03-30 19:13:43 +0900

    fix: ㅙ, 갉 과 같이 모음이나 자음이 연달아 들어가는 경우, 인식하지 못했던 에러 수정

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@ function fixKrEngTypo(editor: Editor) {
 	try {
 		const selected = editor.getSelection()
 		if (!selected) return; // 드레그 된 부분이 없다면 끝
-		const fixed = fixTypo(selected);
+		const fixed = typoFix(selected);
 		editor.replaceSelection(fixed)
 		new Notice("변경되었습니다.")
 	} catch (error) {
@@ -25,112 +25,157 @@ function fixKrEngTypo(editor: Editor) {
 	}
 }
 
-
 const start = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
 const middle = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"];
 const end = [null, "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
 
+const engKrMap: { [key: string]: string } = {
+    "a": "ㅁ", "b": "ㅠ", "c": "ㅊ", "d": "ㅇ", "e": "ㄷ", "f": "ㄹ", "g": "ㅎ", "h": "ㅗ", "i": "ㅑ", "j": "ㅓ",
+    "k": "ㅏ", "l": "ㅣ", "m": "ㅡ", "n": "ㅜ", "o": "ㅐ", "p": "ㅔ", "q": "ㅂ", "r": "ㄱ", "s": "ㄴ", "t": "ㅅ",
+    "u": "ㅕ", "v": "ㅍ", "w": "ㅈ", "x": "ㅌ", "y": "ㅛ", "z": "ㅋ", "E": "ㄸ", "Q": "ㅃ", "R": "ㄲ", "T": "ㅆ",
+    "W": "ㅉ", "O": "ㅒ", "P": "ㅖ"
+};
 
-const engKrMap: { [key: string]: string} = {
-    "a" : "ㅁ", "b" : "ㅠ", "c" : "ㅊ", "d" : "ㅇ", "e" : "ㄷ", "f" : "ㄹ", "g" : "ㅎ", "h" : "ㅗ", "i" : "ㅑ", "j" : "ㅓ",
-    "k" : "ㅏ", "l" : "ㅣ", "m" : "ㅡ", "n" : "ㅜ", "o" : "ㅐ", "p" : "ㅔ", "q" : "ㅂ", "r" : "ㄱ", "s" : "ㄴ", "t" : "ㅅ",
-    "u" : "ㅕ", "v" : "ㅍ", "w" : "ㅈ", "x" : "ㅌ", "y" : "ㅛ", "z" : "ㅋ", "E" : "ㄸ", "Q" : "ㅃ", "R" : "ㄲ", "T" : "ㅆ",
-    "W" : "ㅉ", "O" : "ㅒ", "P" : "ㅖ"
+const specialMap: { [key: string]: string } = {
+    "rt": "ㄳ", "sw": "ㄵ", "sg": "ㄶ", "fr": "ㄺ", "fa": "ㄻ", "fq": "ㄼ",
+    "ft": "ㄽ", "fx": "ㄾ", "fv": "ㄿ", "fg": "ㅀ", "qt": "ㅄ",
+    "hk": "ㅘ", "ho": "ㅙ", "hl": "ㅚ", "nj": "ㅝ", "np": "ㅞ", "nl": "ㅟ", "ml": "ㅢ"
+};
+
+function isConsonantForStartInHangul(alp: string): boolean {
+    return engKrMap[alp] in start;
 }
 
-function fixTypo(inputString: string): string {
-    const hangulList = toHangulList(inputString);
-    const split = splitHangul(hangulList);
-    const concat = split.map((w) => concateHangul(w));
-
-    return concat.join("");
+function isVowelInHangul(alp: string): boolean {
+    if (alp in engKrMap) {
+        return middle.includes(engKrMap[alp]);
+    }
+    if (alp in specialMap) {
+        return middle.includes(specialMap[alp]);
+    }
+    return false;
 }
 
-function toHangulList(engs: string): string {
-	let temp: string[] = [];
-
-	for (let alp of engs) {
-		temp.push(engKrMap[alp] || alp);
-	}
-
-	return temp.join("");
+function alpToHangul(alps: string[]): string[] {
+    return alps.map(alp => engKrMap[alp] || specialMap[alp] || '');
 }
 
-function splitHangul(hanguls: string): string[][] {
-    let splitList: string[][] = [];
+function isSpecial(alp1: string, alp2: string): boolean {
+    return (alp1 + alp2) in specialMap;
+}
+
+function printList(list: string[][]) {
+    for (var l in list) {
+        console.log(l)
+    }
+}
+
+function toHangulSplitList(alps: string[]): string[][] {
+    const splitList: string[][] = [];
     let temp: string[] = [];
 
-    for (let i = 0; i < hanguls.length; i++) {
-        const hangul = hanguls[i];
-
-        if (!isHangul(hangul)) {
-            if (temp.length !== 0) {
-                splitList.push([...temp]);
+    alps.forEach((alp, i) => {
+        if (!(alp in engKrMap)) { // 공백, 특수, 숫자 등
+            if (temp.length) {
+                splitList.push(alpToHangul(temp));
                 temp = [];
             }
-            splitList.push([hangul]);
-            continue;
+            splitList.push([alp]);
+            return;
         }
 
-        if (i === hanguls.length - 1) {
-            temp.push(hangul);
-            splitList.push([...temp]);
-            continue;
+        if (!temp.length) { // 현재 버퍼가 비어있을 때
+            if (isVowelInHangul(alp)) {
+                splitList.push([alp]);
+            } else {
+                temp.push(alp);
+            }
+            return;
         }
 
-        if (temp.length <= 1) {
-            temp.push(hangul);
-            continue;
-        }
+        const lastAlp = temp[temp.length - 1];
 
-        if (!isMiddle(hangul) && !isMiddle(hanguls[i + 1])) {
-            temp.push(hangul);
-            splitList.push([...temp]);
-            temp = [];
-        } else if (!isMiddle(hangul)) {
-            splitList.push([...temp]);
-            temp = [hangul];
-        } else {
-            temp.push(hangul);
+        if (isVowelInHangul(alp)) { // 현재 알파벳이 모음일 때
+            if (temp.length === 1) {
+                if (isVowelInHangul(lastAlp)) {
+                    // 여기에 도달하면 안 됨
+                } else {
+                    temp.push(alp);
+                }
+            } else {
+                if (isVowelInHangul(lastAlp)) { // 마지막이 모음일 때
+                    if (isSpecial(lastAlp, alp)) {
+                        temp[temp.length - 1] = lastAlp + alp;
+                    } else {
+                        splitList.push(alpToHangul(temp));
+                        temp = [alp];
+                    }
+                } else { // 마지막이 자음일 때
+                    temp.pop();
+                    splitList.push(alpToHangul(temp));
+                    temp = [lastAlp, alp];
+                }
+            }
+        } else { // 현재 알파벳이 자음일 때
+            if (temp.length === 1) {
+                splitList.push(alpToHangul(temp));
+                temp = [alp];
+            } else {
+                if (isVowelInHangul(lastAlp)) {
+                    temp.push(alp);
+                } else {
+                    if (isSpecial(lastAlp, alp)) {
+                        if (i < alps.length - 1 && isVowelInHangul(alps[i + 1])) {
+                            splitList.push(alpToHangul(temp));
+                            temp = [alp];
+                        } else {
+                            temp[temp.length - 1] = lastAlp + alp;
+                            splitList.push(alpToHangul(temp));
+                        }
+                    } else {
+                        splitList.push(alpToHangul(temp));
+                        temp = [alp];
+                    }
+                }
+            }
         }
+    });
+
+    if (temp.length) {
+        splitList.push(alpToHangul(temp));
     }
 
     return splitList;
-}
-
-function concateHangul(hangul: string[]): string {
-    if (!onlyHangul(hangul)) {
-        return hangul.join("");
-    }
-
-    if (hangul.length === 2) {
-        return String.fromCharCode((start.indexOf(hangul[0]) * 588) + (middle.indexOf(hangul[1]) * 28) + 44032);
-    }
-
-    if (hangul.length === 3) {
-        return concateHangulBadchim(hangul);
-    }
-
-    return "";
-}
-
-function concateHangulBadchim(hangul: string[]): string {
-    return String.fromCharCode(
-        (start.indexOf(hangul[0]) * 588) +
-        (middle.indexOf(hangul[1]) * 28) +
-        (end.indexOf(hangul[2]) || 0) + 44032
-    );
-}
-
-function onlyHangul(hangul: string[]): boolean {
-    return hangul.every(h => isHangul(h));
 }
 
 function isHangul(c: string): boolean {
     return start.includes(c) || middle.includes(c) || end.includes(c);
 }
 
-function isMiddle(hangul: string): boolean {
-    return middle.includes(hangul);
+function onlyHangul(hangul: string[]): boolean {
+    return hangul.every(h => isHangul(h));
 }
 
+function concatHangul(hangul: string[]): string {
+    if (!onlyHangul(hangul)) {
+        return hangul.join('');
+    }
+    if (hangul.length === 1) {
+        return hangul[0];
+    }
+    if (hangul.length === 3) {
+        return concatHangulWithBatchim(hangul);
+    }
+    return String.fromCharCode((start.indexOf(hangul[0]) * 588) + (middle.indexOf(hangul[1]) * 28) + 44032);
+}
+
+function concatHangulWithBatchim(hangul: string[]): string {
+    return String.fromCharCode((start.indexOf(hangul[0]) * 588) + (middle.indexOf(hangul[1]) * 28) + (end.indexOf(hangul[2]) || 0) + 44032);
+}
+
+function typoFix(inputString: string): string {
+    const hangulList = toHangulSplitList([...inputString]);
+    const concat = hangulList.map(w => concatHangul(w));
+
+    return concat.join('');
+}
